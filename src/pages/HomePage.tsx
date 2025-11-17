a6c58c72a8b5070188e8426a493665bbdfcaf40f
Rene Krallis <160429355+renelk0208@users.noreply.github.com>
2025-11-17 12:14:36 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,21 @@
+import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import CertificatesBanner from '../components/CertificatesBanner';
 import About from '../components/About';
 import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
 
 export default function HomePage() {
   return (
     <>
+      <ScrollToTop />
+      <Navigation />
       <Hero />
       <CertificatesBanner />
       <About />
       <Contact />
+      <Footer />
     </>
   );
 }
